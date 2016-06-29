e8ae12ed7b2abbaebeffe9cf3ae5f1e91b0b9264
eddie <oakes1218@hotmail.com>
2016-06-29 15:44:02 +0000

    update lotto

--- homework/HomeworkLotto.php
+++ homework/HomeworkLotto.php
@@ -1,21 +1,43 @@
 <?php
 function lotto()
 {
+    //建立1~49順序陣列
     for($i = 1 ; $i < 50; $i ++)
     {
         $arrayRandom[] = $i;
     }
+    //將陣列洗牌
     shuffle($arrayRandom);
     
-    //print_r($arrayRandom);
-      
+    //取出洗牌後前六個數組  
     for($i = 0; $i < 6; $i++)
     {
         $arrayResult[] = $arrayRandom[$i];
-        echo " $arrayRandom[$i] ,";
+
     }
+    echo "本期壓注號碼為: ";
+    foreach ($_GET as $value) {
+        echo  "$value       ";
+    }
+    
+    echo "本期中獎號碼為: ";
+    foreach ($arrayResult as $value) {
+        echo  "$value       ";
+    }
+    //將隨機產生數組 與 form 輸入數組升謐排列
+    asort($arrayResult);
+    asort($_GET);
+   
+    //比較升謐排列輸入與產生數組比對
+    $result = array_diff($arrayResult,$_GET);
+     "<br>" ;
+    //判斷陣列是否有完全相同 得知有無中獎
+    if($result){
+        echo "<br> 沒中獎";
+    }else
+        echo "<br> 恭喜中獎";
+    
 }    
-lotto();
 
 ?>
 <!DOCTYPE html>
@@ -25,12 +47,17 @@ lotto();
         <title>樂透開獎號碼</title>
     </head>
     <body>
-        <input type="text" , size="5", name="one"/>
-        <input type="text" , size="5", name="two"/>
-        <input type="text" , size="5", name="three"/>
-        <input type="text" , size="5", name="four"/>
-        <input type="text" , size="5", name="five"/>
-        <input type="text" , size="5", name="six"/> <br>
-        <input type="submit" , size="15"/>
+        <form action = "">
+            <div>請輸入1~49 中任意6個數字</div>
+            <input type="text" , size="5", name="one"/>
+            <input type="text" , size="5", name="two"/>
+            <input type="text" , size="5", name="three"/>
+            <input type="text" , size="5", name="four"/>
+            <input type="text" , size="5", name="five"/>
+            <input type="text" , size="5", name="six"/> 
+            <input type="submit" , size="15"/>
+         </form>
+        
+         <?php lotto(); ?>
     </body>
 </html>
