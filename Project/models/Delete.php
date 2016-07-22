3c0167e38f8583985c0bfeb3dbe0e50da0a33297
eddie <oakes1218@hotmail.com>
2016-07-22 17:04:41 +0000

    delete file function

--- Project/models/Delete.php
+++ Project/models/Delete.php
@@ -4,9 +4,16 @@ class Delete{
     
     function deleteProduct()
     {
-        //get取直刪除
+        //get取直刪除資料
+        $cmds = "SELECT Picture FROM products WHERE id = $_GET[id]";
+        //get取直刪除DB
         $cmd = "DELETE FROM products WHERE id = $_GET[id]";
         $cf = new Config();
+        $result = $cf->config($cmds);
+        
+        while($row = mysql_fetch_assoc($result)){
+            unlink("picture/".$row["Picture"]);//將檔案刪除
+        }
         $result = $cf->config($cmd);
         header("location: homePage");
     }
