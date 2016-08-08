2cd94daca7d406ed8f5464d0acb8f0c6e055fef6
eddie <oakes1218@hotmail.com>
2016-08-08 05:18:07 +0000

    0 1 prictice version1

--- practice.php
+++ practice.php
@@ -1,6 +1,9 @@
 <?php
-    //function practice(){
-        $origin = array(
+class Practice{
+    var $total = array();
+    var $position = array();
+    var $index = 0;
+    var $origin = array(
                         array(1, 1, 0, 0, 0, 0, 0, 0, 0, 0),
                         array(1, 1, 0, 1, 1, 0, 0, 0, 0, 0),
                         array(0, 0, 0, 1, 1, 0, 0, 0, 0, 0),
@@ -12,37 +15,68 @@
                         array(1, 0, 0, 0, 1, 0, 1, 1, 1, 1),
                         array(1, 1, 0, 1, 1, 0, 0, 0, 0, 1)
                         );
-        //echo $origin[0][0];
+    var $origin1 = array(
+                        array(1, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+                        array(1, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+                        array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                        array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                        array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                        array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                        array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                        array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                        array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                        array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+                        );
+    function action(){
+        
         for($j = 0 ; $j < 10; $j++){
             for($i = 0 ; $i < 10; $i++){
-                
-                if($origin[$j][$i] == 1){
-                    $position[] = array($j,$i);
-                    
-                    //$origin[$j][$i-1] ."<br>"; //左
-                    
-                    $origin[$j+1][$i] ."<br>"; //下
-                    $origin[$j][$i+1] == 1 ."<br>"; //右
-                    // $origin[$j-1][$i] ."<br>"; //上
+                if($this->origin[$j][$i] == 1){
+                    echo $j."/".$i;
+                    $this->read($j, $i);
+                    // $this->index ++;
                 }
-                // $a = $origin[$j+1][$i] + $origin[$j-1][$i] + $origin[$j][$i+1] + $origin[$j][$i-1];
-                // $countArray[] = $a ;
-                
-                // echo $a;
-               
             }    
         }
-        var_dump($position);
-        // var_dump($countArray);
-        function read($num)
-        {
-        echo $num . "<Br>";
-        if ($num>100) //設定遞迴條件
-          {
-           return; //結束遞迴
-          }
-        read($num+1); //呼叫函數自己  
+        // var_dump($this->position);
+        print_r($this->position);
+    }
+    
+    function read($j, $i){
+        
+        $this->origin[$j][$i] =0;
+        // $this->position = array($j,$i);
+        array_push($this->position,array($j,$i));
+        
+        if($this->origin[$j][$i+1] == 1){
+            $this->read($j,$i+1);
         }
-        //read(10);
-   
+        if($this->origin[$j+1][$i] == 1){
+            $this->read($j+1,$i);
+        }
+        if($this->origin[$j][$i-1] == 1){
+            $this->read($j,$i-1);
+        }
+        if($this->origin[$j-1][$i] == 1){
+            $this->read($j-1,$i);
+        }
+    }   
+    
+    function drawing(){
+        for($j = 0 ; $j < 10; $j++){
+            echo "<tr>"; 
+            for($i = 0 ; $i < 10; $i++){
+                echo "<td>".$this->origin[$j][$i]."</td>";
+            }
+            echo "</tr>";
+        }
+    }
+            
+    
+}
+$p = new Practice();
+echo ' <table width="300" border="1"> ';
+$p->drawing();
+echo ' </table> ';
+$p->action();
 ?>
