84e9c219f3482e49ce14af76dce002f3c2ed5c4f
eddie <oakes1218@hotmail.com>
2016-08-10 15:00:06 +0000

    01 practice finsh

--- practice.php
+++ practice.php
@@ -16,8 +16,8 @@ class Practice{
                         array(1, 1, 0, 1, 1, 0, 0, 0, 0, 1)
                         );
     var $origin1 = array(
-                        array(1, 1, 0, 0, 0, 0, 0, 0, 0, 0),
-                        array(1, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+                        array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                        array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
                         array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
                         array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
                         array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
@@ -27,7 +27,7 @@ class Practice{
                         array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
                         array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
                         );
-    //取得區塊各點座標 放置total array                    
+    //取得區塊各點座標 放置total array
     function action(){
         $index = 0;
         $max = array();
@@ -35,40 +35,81 @@ class Practice{
             for ($i = 0 ; $i < 10; $i++) {
                 if ($this->origin[$j][$i] == 1) {
                     $this->read($j, $i);
-                    echo $index ."<hr>";
                     $this->total[$index] = $this->position;
                     //將為一區塊座標清空
                     $this->position = array();
                     $index++;
                 }
-            }    
+            }
         }
         //找出最大區塊者
-        for ($i = 0 ; $i < count($this->total) ; $i++) {
-            
+        for ($i = 0; $i < count($this->total); $i++) {
             $max[$i] = count($this->total[$i]);
-            echo count($this->total[$i]);
-            echo "<hr>";
         }
+
+        //找最大區塊做標
         $maxPosition = max($max);
-        for ($i = 0 ; $i < count($this->total) ; $i++) {
+        for ($i = 0; $i < count($this->total); $i++) {
             if (count($this->total[$i]) == $maxPosition) {
                 $result = $this->total[$i];
             }
         }
-        // return $result;
 
+        //建立比較陣列
+        for ($j = 0; $j < 10; $j++) {
+            for ($i = 0; $i < 10; $i++) {
+                if ($this->origin1[$j][$i] == 0) {
+                    $comparison[] = array($j, $i);
+                }
+            }
+        }
 
+        //找出該座標點寫1
+        for ($j = 0; $j < 13; $j++) {
+            for ($i = 0; $i < 100; $i++) {
+                if ($result[$j] == $comparison[$i]) {
+                     $comparison[$i] = 1;
+                }
+            }
+        }
 
-        print_r($result[0]);
+        //將其餘座標寫0
+        for ($i = 0; $i < 100; $i++) {
+            if ($comparison[$i] == 1) {
+                 $comparison[$i] = 1;
+            } else {
+                $comparison[$i] = 0;
+            }
+        }
+
+        for ($j = 0; $j < 10; $j++) {
+            for ($i = 0; $i < 10; $i++) {
+                if ($comparison[$j * 10 + $i] == 1) {
+                    $finsh[$j][$i] = 1;
+                } else {
+                    $finsh[$j][$i] = 0;
+                }
+            }
+        }
+
+        //畫出結果圖
+        echo "<hr>";
+        echo ' <table width="300" border="1"> ';
+        for ($j = 0 ; $j < 10; $j++) {
+            echo "<tr>";
+            for ($i = 0 ; $i < 10; $i++) {
+                echo "<td>".$finsh[$j][$i]."</td>";
+            }
+            echo "</tr>";
+        }
+        echo ' </table> ';
     }
     //遞迴右下左上找值
     function read($j, $i){
-        
+
         $this->origin[$j][$i] =0;
-        // $this->position = array($j,$i);
         array_push($this->position,array($j,$i));
-        
+
         if ($this->origin[$j][$i+1] == 1) {
             $this->read($j,$i+1);
         }
@@ -81,24 +122,23 @@ class Practice{
         if ($this->origin[$j-1][$i] == 1) {
             $this->read($j-1,$i);
         }
-        
-        
-    }   
-    
+    }
+
     function drawing(){
         for ($j = 0 ; $j < 10; $j++) {
-            echo "<tr>"; 
+            echo "<tr>";
             for ($i = 0 ; $i < 10; $i++) {
                 echo "<td>".$this->origin[$j][$i]."</td>";
             }
             echo "</tr>";
         }
     }
-    
 }
+
 $p = new Practice();
 echo ' <table width="300" border="1"> ';
 $p->drawing();
 echo ' </table> ';
+
 $p->action();
 ?>
